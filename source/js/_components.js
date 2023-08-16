ce69b8d822fad49943deb77d8337b354e0820b2c
Roman <romanshliakhov@gmail.com>
2023-08-16 17:02:50 +0300

    add header

--- source/js/_components.js
+++ source/js/_components.js
@@ -17,10 +17,10 @@
 // import './components/mobileMenu.js';
 // import './components/filters.js';
 // import './components/filtersSearch.js';
-import './components/sliders.js';
-import './components/smoothScroll.js';
-import './components/scrollHeader.js';
-import './components/lineAnimation.js';
+// import './components/sliders.js';
+// import './components/smoothScroll.js';
+// import './components/scrollHeader.js';
+// import './components/lineAnimation.js';
 
 
 
